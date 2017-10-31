311ee0a4636c00428517b4c9a534903e162361da
Grant <grantcomiller@gmail.com>
2017-10-31 16:39:48 +0800

    Initial Commit

---
+++ EconSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EconSim
+//
+//  Created by Grant Miller on 10/31/17.
+//  Copyright © 2017 Grant Miller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
